b4f1f9e0c6039848f06a71573bf2f460b171c4d0
ileenamitra <ileenamitra@gmail.com>
2020-07-02 16:25:56 -0700

    test script for chr X

--- test_chrX.sh
+++ test_chrX.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# should test naive and not naive
+### test naive for expansions
 ./src/CookieMonSTR \
     --chrX \
-    --strvcf pg_quad_NA12879_82_hg19_chrX.sorted.vcf.gz \
+    --region chrX:0-34026969 \
+    --strvcf /storage/ileena/pg/gangstr_chrX/pg_quad_NA12879_82_hg19_chrX.sorted.vcf.gz \
     --fam family.ped \
     --max-num-alleles 100 \
     --round-alleles --include-invariant \
@@ -13,4 +14,20 @@
     --naive \
     --naive-expansions-frr 3,8 \
     --min-num-encl-child 3 --max-perc-encl-parent 0.05 --min-encl-match 0.9 --min-total-encl 10 \
-    --out test_results.txt --verbose --debug
+    --out test_naive_results --verbose --debug
+
+### test model based
+# ./src/CookieMonSTR \
+#     --chrX \
+#     --region chrX:0-34026969 \
+#     --strvcf /storage/ileena/pg/gangstr_chrX/pg_quad_NA12879_82_hg19_chrX.sorted.vcf.gz \
+#     --fam family.ped \
+#     --max-num-alleles 100 \
+#     --round-alleles --include-invariant \
+#     --output-all-loci \
+#     --gangstr \
+#     --require-all-children \
+#     --default-prior -3 \
+#     --posterior-threshold 0.5 \
+#     --min-num-encl-child 3 --max-perc-encl-parent 0.05 --min-encl-match 0.9 --min-total-encl 10 \
+#     --out test_model_results --verbose --debug
